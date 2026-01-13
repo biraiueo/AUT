acde2f062a1a5488ccc3097dbed06d52f04f47f6
Nabira Ananda <nabira@example.com>
2026-01-13 15:54:16 +0700

    update script hello_aut

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -4,13 +4,17 @@ from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         options = webdriver.FirefoxOptions()
-        options.add_argument('--ignore-ssl-errors-yes')
+        options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
-        server = 'http://localhost:4444'
+        options.add_argument('--headless')
 
-        self.browser = webdriver.Remote(command_executor=server, options=options)
+        server = 'http://localhost:4444'
+        self.browser = webdriver.Remote(
+            command_executor=server,
+            options=options
+        )
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
@@ -20,10 +24,12 @@ class AutTest(unittest.TestCase):
             url = "http://localhost"
 
         self.browser.get(url)
+
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
 
         self.assertIn(expected_result, actual_result.text)
 
-    if __name__ == '__main__':
-        unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
